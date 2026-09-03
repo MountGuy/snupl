c7c65e16608c43007da95b2144986b612dd2e9b2
MountGuy <riman314@snu.ac.kr>
2026-09-03 16:51:47 +0900

    commit

--- src/automata.h
+++ src/automata.h
@@ -1,5 +1,6 @@
 #include "common.h"
 
-void add_resource(char *string, SType stype, CParser *c_parser);
 void gather_strings(GParser *parser, CParser *c_parser);
+void add_resource(char *string, SType stype, CParser *c_parser);
+void sort_resource(CParser *parser);
 void _gather_strings(GExpr *expr, SType stype, CParser *c_parser);
