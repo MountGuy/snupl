c24f647fc521a3c4be09aaff213d038a9761e7e2
MountGuy <riman314@snu.ac.kr>
2026-09-18 12:35:34 +0900

    commit

--- src/chunk.h
+++ src/chunk.h
@@ -10,12 +10,17 @@
 
 Chunk init_chunk(size_t unit, int expands);
 void expand_chunk(Chunk *chunk);
+
 void *append_data(void *source, int length, Chunk *chunk);
 void *alloc_mem(int length, Chunk *chunk);
+
 void write_data(void *source, int idx, Chunk *chunk);
 void write_last(void *source, Chunk *chunk);
 void read_data(void *dest, int idx, Chunk *chunk);
 void read_last(void *dest, Chunk *chunk);
+
+int has_space(int length, Chunk *chunk);
+
 void *fix_chunk(Chunk *chunk);
 void del_chunk(Chunk *chunk);
 
